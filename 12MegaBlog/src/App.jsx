d4e44c70e688b9876f691c58b3a46eba7a88fa6c
Rampawara99 <ramjadhaw1999@gmail.com>
2024-05-18 23:46:25 +0530

    12 mega blog project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+function App() {
+  // console.log("env: ", process.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <h1 className="bg-gray-400 text-center font-semibold">Hello Vite + React!</h1>
+    </>
+  )
+}
+
+export default App
